a771adb52795af6c0a63200bc1a88ade6ee29f4d
htortosa <44167657+htortosa@users.noreply.github.com>
2021-06-30 11:50:29 +0200

    Update crypto-price-app.py

--- crypto-price-app.py
+++ crypto-price-app.py
@@ -1,4 +1,4 @@
-# This app is for educational purpose only. Insights gained is not financial advice. Use at your own risk!
+# # This app is for educational purpose only. Insights gained is not financial advice. Use at your own risk!
 import streamlit as st
 from PIL import Image
 import pandas as pd
@@ -15,7 +15,7 @@ import time
 #---------------------------------#
 # Page layout
 ## Page expands to full width
-st.beta_set_page_config(layout="wide")
+st.set_page_config(layout="wide") #st.beta_set_page_config(layout="wide")
 #---------------------------------#
 # Title
 
@@ -77,21 +77,21 @@ def load_data():
       coin_name.append(i['slug'])
       coin_symbol.append(i['symbol'])
       price.append(i['quote'][currency_price_unit]['price'])
-      percent_change_1h.append(i['quote'][currency_price_unit]['percent_change_1h'])
-      percent_change_24h.append(i['quote'][currency_price_unit]['percent_change_24h'])
-      percent_change_7d.append(i['quote'][currency_price_unit]['percent_change_7d'])
-      market_cap.append(i['quote'][currency_price_unit]['market_cap'])
-      volume_24h.append(i['quote'][currency_price_unit]['volume_24h'])
+      percent_change_1h.append(i['quote'][currency_price_unit]['percentChange1h']) # percent_change_1h
+      percent_change_24h.append(i['quote'][currency_price_unit]['percentChange24h']) #percent_change_24h
+      percent_change_7d.append(i['quote'][currency_price_unit]['percentChange7d']) # percent_change_7d
+      market_cap.append(i['quote'][currency_price_unit]['marketCap']) # market_cap
+      volume_24h.append(i['quote'][currency_price_unit]['volume24h']) # volume_24h
 
-    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'])
+    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'marketCap', 'percentChange1h', 'percentChange24h', 'percentChange7d', 'price', 'volume24h'])
     df['coin_name'] = coin_name
     df['coin_symbol'] = coin_symbol
     df['price'] = price
-    df['percent_change_1h'] = percent_change_1h
-    df['percent_change_24h'] = percent_change_24h
-    df['percent_change_7d'] = percent_change_7d
-    df['market_cap'] = market_cap
-    df['volume_24h'] = volume_24h
+    df['percentChange1h'] = percent_change_1h
+    df['percentChange24h'] = percent_change_24h
+    df['percentChange7d'] = percent_change_7d
+    df['marketCap'] = market_cap
+    df['volume24h'] = volume_24h
     return df
 
 df = load_data()
@@ -109,7 +109,7 @@ df_coins = df_selected_coin[:num_coin]
 ## Sidebar - Percent change timeframe
 percent_timeframe = col1.selectbox('Percent change time frame',
                                     ['7d','24h', '1h'])
-percent_dict = {"7d":'percent_change_7d',"24h":'percent_change_24h',"1h":'percent_change_1h'}
+percent_dict = {"7d":'percentChange7d',"24h":'percentChange24h',"1h":'percentChange1h'}
 selected_percent_timeframe = percent_dict[percent_timeframe]
 
 ## Sidebar - Sorting values
@@ -133,11 +133,11 @@ col2.markdown(filedownload(df_selected_coin), unsafe_allow_html=True)
 #---------------------------------#
 # Preparing data for Bar plot of % Price change
 col2.subheader('Table of % Price Change')
-df_change = pd.concat([df_coins.coin_symbol, df_coins.percent_change_1h, df_coins.percent_change_24h, df_coins.percent_change_7d], axis=1)
+df_change = pd.concat([df_coins.coin_symbol, df_coins.percentChange1h, df_coins.percentChange24h, df_coins.percentChange7d], axis=1)
 df_change = df_change.set_index('coin_symbol')
-df_change['positive_percent_change_1h'] = df_change['percent_change_1h'] > 0
-df_change['positive_percent_change_24h'] = df_change['percent_change_24h'] > 0
-df_change['positive_percent_change_7d'] = df_change['percent_change_7d'] > 0
+df_change['positive_percent_change_1h'] = df_change['percentChange1h'] > 0
+df_change['positive_percent_change_24h'] = df_change['percentChange24h'] > 0
+df_change['positive_percent_change_7d'] = df_change['percentChange7d'] > 0
 col2.dataframe(df_change)
 
 # Conditional creation of Bar plot (time frame)
@@ -145,25 +145,25 @@ col3.subheader('Bar plot of % Price Change')
 
 if percent_timeframe == '7d':
     if sort_values == 'Yes':
-        df_change = df_change.sort_values(by=['percent_change_7d'])
+        df_change = df_change.sort_values(by=['percentChange7d'])
     col3.write('*7 days period*')
     plt.figure(figsize=(5,25))
     plt.subplots_adjust(top = 1, bottom = 0)
-    df_change['percent_change_7d'].plot(kind='barh', color=df_change.positive_percent_change_7d.map({True: 'g', False: 'r'}))
+    df_change['percentChange7d'].plot(kind='barh', color=df_change.positive_percent_change_7d.map({True: 'g', False: 'r'}))
     col3.pyplot(plt)
 elif percent_timeframe == '24h':
     if sort_values == 'Yes':
-        df_change = df_change.sort_values(by=['percent_change_24h'])
+        df_change = df_change.sort_values(by=['percentChange24h'])
     col3.write('*24 hour period*')
     plt.figure(figsize=(5,25))
     plt.subplots_adjust(top = 1, bottom = 0)
-    df_change['percent_change_24h'].plot(kind='barh', color=df_change.positive_percent_change_24h.map({True: 'g', False: 'r'}))
+    df_change['percentChange24h'].plot(kind='barh', color=df_change.positive_percent_change_24h.map({True: 'g', False: 'r'}))
     col3.pyplot(plt)
 else:
     if sort_values == 'Yes':
-        df_change = df_change.sort_values(by=['percent_change_1h'])
+        df_change = df_change.sort_values(by=['percentChange1h'])
     col3.write('*1 hour period*')
     plt.figure(figsize=(5,25))
     plt.subplots_adjust(top = 1, bottom = 0)
-    df_change['percent_change_1h'].plot(kind='barh', color=df_change.positive_percent_change_1h.map({True: 'g', False: 'r'}))
+    df_change['percentChange1h'].plot(kind='barh', color=df_change.positive_percent_change_1h.map({True: 'g', False: 'r'}))
     col3.pyplot(plt)
